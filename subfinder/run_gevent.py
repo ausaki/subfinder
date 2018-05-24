085a0276ae4613977fde4210d47923704ad98d81
lujiamin <lujiamin@lujiamin.com>
2018-05-24 19:25:01 +0800

    fix script bug

--- subfinder/run_gevent.py
+++ subfinder/run_gevent.py
@@ -1,7 +1,12 @@
 # -*- coding: utf8 -*-
 from gevent import monkey;monkey.patch_all()
-from .run import run
+from .run import run as run_
 from .subfinder_gevent import SubFinderGevent as SubFinder
 
+
+def run():
+    run_(SubFinder)
+
+
 if __name__ == '__main__':
-    run(SubFinder)
+    run()
