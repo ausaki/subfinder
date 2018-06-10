cfaea30327548aa0124ad99a2a67b830169ca076
lujiamin <lujiamin@lujiamin.com>
2018-06-10 21:28:57 +0800

    add mv_videos util

--- subfinder/utils.py
+++ subfinder/utils.py
@@ -21,23 +21,52 @@ def rm_subtitles(path):
     return count
 
 
+def mv_videos(path):
+    """ move videos in sub-directory of path to path.
+    """
+    count = 0
+    for f in os.listdir(path):
+        f = os.path.join(path, f)
+        if os.path.isdir(f):
+            for sf in os.listdir(f):
+                sf = os.path.join(f, sf)
+                if os.path.isfile(sf):
+                    new_name = os.path.join(path, os.path.basename(sf))
+                    os.rename(sf, new_name)
+                    count += 1
+                    print('mv {} to {}'.format(sf, new_name))
+    return count
+
+
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--delete-subs',
-                        help="delete all subtitles in PATH recursively.the PATH defaults to CWD")
+                        help="delete all subtitles in PATH recursively.")
+    parser.add_argument('-m', '--move-videos',
+                        help="move all files(like videos, subtitles) in sub-directory of PATH to PATH.")
 
     args = parser.parse_args()
 
-    # try to decode str to unicode in python2
-    try:
-        args.delete_subs = args.delete_subs.decode(sys.getfilesystemencoding())
-    except AttributeError as e:
-        pass
-
     if args.delete_subs:
+        # try to decode str to unicode in python2
+        try:
+            args.delete_subs = args.delete_subs.decode(sys.getfilesystemencoding())
+        except AttributeError as e:
+            pass
         print('Start delete subtitles in {}'.format(args.delete_subs))
         c = rm_subtitles(args.delete_subs)
         print('Done, delete {} subtitles'.format(c))
+
+    if args.move_videos:
+        # try to decode str to unicode in python2
+        try:
+            args.move_videos = args.move_videos.decode(sys.getfilesystemencoding())
+        except AttributeError as e:
+            pass
+        print('Start move videos in {}'.format(args.move_videos))
+        c = mv_videos(args.move_videos)
+        print('Done, move {} files'.format(c))
+
 
 if __name__ == '__main__':
     main()
