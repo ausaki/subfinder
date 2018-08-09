bdd1796429a4fbebd41c278b565572b3e71abce3
lujiamin <lujiamin@lujiamin.com>
2018-08-09 22:34:09 +0800

    packaging

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
-
 import re
 try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+import six
 from subfinder import __version__, __author__
 version = __version__
 author = __author__
@@ -15,28 +15,45 @@ if not version:
 if not author:
     raise RuntimeError('Cannot find author information')
 
-requires = []
-with open('requirements.txt') as fp:
-    for line in fp:
-        line = line.strip()
-        if line.startswith('#'):
-            continue
-        requires.append(line)
+requires = [
+    'requests',
+    'lxml',
+    'bs4',
+    'gevent',
+    'rarfile',
+    'six',
+]
 
-setup(name='SubFinder',
-      version=version,
-      description='subfinder -- a general finder for subtitles',
-      author=author,
-      author_email='ljm51689@gmail.com',
-      url='https://github.com/ausaki/subfinder/',
-      license='MIT',
-      packages=['subfinder', 'subfinder.subsearcher'],
-      include_package_data=True,
-      entry_points={
-            'console_scripts': [
-                  'subfinder = subfinder.run_gevent:run',
-                  'subutils = subfinder.utils:main'
-            ]
-      },
-      install_requires=requires,
-    )
+
+def readme():
+    if six.PY2:
+        with open('README.md') as fp:
+            content = fp.read()
+    else:
+        with open('README.md', encoding='utf-8') as fp:
+            content = fp.read()
+    return content
+
+
+setup(
+    name='SubFinder',
+    version=version,
+    author=author,
+    author_email='ljm51689@gmail.com',
+    description='subfinder -- a general finder for subtitles',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/ausaki/subfinder/',
+    keywords=['subtitle', 'subfinder', 'sub', 'subtitle finder', 'shooter', 'zimuku'],
+    license='MIT',
+    packages=['subfinder', 'subfinder.subsearcher'],
+    include_package_data=True,
+    python_requires='>=2.7',
+    install_requires=requires,
+    entry_points={
+        'console_scripts': [
+                'subfinder = subfinder.run_gevent:run',
+                'subutils = subfinder.utils:main'
+        ]
+    },
+)
